32cc3772b42d7900a95d02f81ca2b83c83051232
manojmogalla <manojmvvsai@gmail.com>
2022-02-02 08:29:54 +0530

    add

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class AuthService {
-   login = '/v1/labs/labs-login';
+   login = '/labs/labs-login';
    otpverify = '/labs/otp-verification';
    forgotpassword='/labs/labs-forgot-password';
    resetpassword='/labs/reset-password';
