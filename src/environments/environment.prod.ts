990395027e7e2bcc04b21e5d913c17b30e2757ad
manojmogalla <manojmvvsai@gmail.com>
2022-02-02 13:20:02 +0530

    ss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://164.52.209.69/totalhealthbazar/backend/web/index.php/v1'
+  baseUrl: 'https://164.52.209.69/totalhealthbazar/backend/web/index.php/v1'
 
 };
